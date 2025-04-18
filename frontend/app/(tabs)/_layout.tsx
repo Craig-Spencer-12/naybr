6e7640035af9d1ef50ccc12e2c6d57e009992563
Craig Spencer <craig.spencer@intel.com>
2025-04-18 15:34:34 -0400

    get image on frontend

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -47,6 +47,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="image"
+        options={{
+          title: 'Image',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
