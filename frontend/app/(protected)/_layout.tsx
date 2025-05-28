3e22c67987e1b52fef572f971637f9e33dc47cd5
Craig Spencer <craig.spencer@intel.com>
2025-05-28 15:08:33 -0400

    asdf

--- frontend/app/(protected)/_layout.tsx
+++ frontend/app/(protected)/_layout.tsx
@@ -2,17 +2,5 @@ import { useSession } from '@/utils/authContext'
 import { Redirect, Stack } from 'expo-router'
 import 'react-native-reanimated'
 
-export default function ProtectedLayout() {
-
-  const { session } = useSession()
-
-  if (!session.isLoggedIn) {
-    return <Redirect href="/login" />
-  }
-
-  return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
-  )
+ex
 }
