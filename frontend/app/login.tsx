f0de4e292868dc360737ba9879bd2135f94af715
Craig Spencer <craig.spencer@intel.com>
2025-05-28 15:08:09 -0400

    asdf

--- frontend/app/login.tsx
+++ frontend/app/login.tsx
@@ -5,12 +5,6 @@ import { Button, View } from 'react-native'
 export default function LoginScreen() {
     const { logIn } = useSession()
 
-    return (
-        <View>
-            <ThemedText type='subtitle'>Login Screen</ThemedText>
-            <Button title='Login' onPress={() => logIn('3f07b805-d67c-4b8b-b214-bc72ca75ed78')}></Button>
-        </View>
-    )
 }
 
 // TODO replace hard coded id with login flow
